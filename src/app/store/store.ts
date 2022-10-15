7a4f8748952d990ec524581ca23b1bfb4a9fb283
MaksimSolo <taezhnyi@gmail.com>
2022-10-15 18:29:17 +0300

    refactor: types in store & <Provider> in index.tsx

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,7 +1,11 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-// eslint-disable-next-line camelcase
-import { Action, applyMiddleware, combineReducers, legacy_createStore } from 'redux';
-import thunk, { ThunkAction } from 'redux-thunk';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import {
+  Action,
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from 'redux';
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk';
 
 import { appReducer } from './app-reducer';
 
@@ -9,7 +13,12 @@ const rootReducer = combineReducers({
   app: appReducer,
 });
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+// types
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
@@ -19,7 +28,7 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   Action
 >;
 
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
+export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, Action>;
 
 // @ts-ignore
 window.store = store;
