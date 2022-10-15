10f199f47cf2ce2c56927b8c63934849a2946cae
MaksimSolo <taezhnyi@gmail.com>
2022-10-15 13:08:41 +0300

    refactor: change text

--- src/features/auth/registration/Registration.tsx
+++ src/features/auth/registration/Registration.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { ReturnComponentType } from '../../../types';
 
 export const Registration = (): ReturnComponentType => {
-  return <div>Registration</div>;
+  return <div>Registration HUUUURAAAHHH!!!!</div>;
 };
