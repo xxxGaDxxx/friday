584c56efb9c1c86fc008dd95da783c0e17ccc2f3
Pavel Morozuk <morozuki@mail.ru>
2022-10-18 01:29:48 +0300

    refactor: rename Error404.module.css

--- src/features/pageNotFound/PageNotFound.tsx
+++ src/features/pageNotFound/PageNotFound.tsx
@@ -7,7 +7,7 @@ import svgError from '../../assets/svg/error/404.svg';
 import Button from '../../common/button/Button';
 import { ReturnComponentType } from '../../types';
 
-import s from './styles/Error404.module.css';
+import s from './styles/PageNotFound.module.css';
 
 export const PageNotFound = (): ReturnComponentType => {
   const navigate = useNavigate();
