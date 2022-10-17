ec0a0cd633f8937d6019fd608a12af8333cc7f00
Pavel Morozuk <morozuki@mail.ru>
2022-10-18 01:27:47 +0300

    refactor: delete unused reportWebVitals.ts

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { HashRouter } from 'react-router-dom';
 
 import App from './app/App';
 import { store } from './app/store/store';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -18,8 +17,3 @@ root.render(
     </Provider>
   </HashRouter>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
