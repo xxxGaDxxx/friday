f7babf6b4735de2341b4f4c08766e66733e1e0e9
Wlad loban <qwegadqwe@gmail.com>
2022-10-22 11:45:46 +0300

    refactor: structure add

---
+++ src/features/table/cardTable/CardTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ReturnComponentType } from '../../../types';
+
+const CardTable = (): ReturnComponentType => {
+  return <div>CardTable</div>;
+};
+
+export default CardTable;
---
+++ src/features/table/stakeTable/StakeTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ReturnComponentType } from '../../../types';
+
+const StakeTable = (): ReturnComponentType => {
+  return <div>StakeTable</div>;
+};
+
+export default StakeTable;
